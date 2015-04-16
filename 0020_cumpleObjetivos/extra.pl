a4b971687a39e65c8cdf0f68ec07dfb782e4f5ae
Daniel Larrea <argdaniel.larrea@gmail.com>
2015-04-16 11:40:29 -0300

    Revert "Tests del punto 2"
    This reverts commit e70825423465a3b3cc35e41906b53fb9b48ed553.

--- 0020_cumpleObjetivos/extra.pl
+++ 0020_cumpleObjetivos/extra.pl
@@ -2,7 +2,6 @@ continente(americaDelSur).
 continente(americaDelNorte).
 continente(asia).
 continente(oceania).
-continente(europa).
 
 estaEn(americaDelSur, argentina).
 estaEn(americaDelSur, brasil).
@@ -20,12 +19,10 @@ estaEn(oceania,australia).
 estaEn(oceania,sumatra).
 estaEn(oceania,java).
 estaEn(oceania,borneo).
-estaEn(europa,italia).
 
 jugador(amarillo).
 jugador(magenta).
 jugador(negro).
-jugador(blanco).
 
 ocupa(argentina, magenta).
 ocupa(chile, negro).
@@ -43,39 +40,9 @@ ocupa(australia, negro).
 ocupa(sumatra, negro).
 ocupa(java, negro).
 ocupa(borneo, negro).
-ocupa(italia, blanco).
-
-limitrofes(china,kamtchatka).
-limitrofes(japon,china).
-limitrofes(japon,kamtchatka).
-limitrofes(australia,sumatra).
-limitrofes(australia,java).
-limitrofes(australia,borneo).
-limitrofes(australia,chile).
-
-objetivo(amarillo, ocuparContinente(asia)).
-objetivo(amarillo,ocuparPaises(2, americaDelSur)). 
-objetivo(rojo, destruirJugador(negro)). 
-objetivo(magenta, destruirJugador(rojo)). 
-objetivo(negro, ocuparPaises(2, oceania)).
-objetivo(blanco, ocuparContinente(europa)). 
-
-cuantosPaisesOcupaEn(amarillo, americaDelSur, 1).
-cuantosPaisesOcupaEn(amarillo, americaDelNorte, 4).
-cuantosPaisesOcupaEn(amarillo, asia, 3).
-cuantosPaisesOcupaEn(amarillo, oceania, 0).
-cuantosPaisesOcupaEn(magenta, americaDelSur, 2).
-cuantosPaisesOcupaEn(magenta, americaDelNorte, 0).
-cuantosPaisesOcupaEn(magenta, asia, 0).
-cuantosPaisesOcupaEn(magenta, oceania, 0).
-cuantosPaisesOcupaEn(negro, americaDelSur, 1).
-cuantosPaisesOcupaEn(negro, americaDelNorte, 0).
-cuantosPaisesOcupaEn(negro, asia, 1).
-cuantosPaisesOcupaEn(negro, oceania, 4).
 
 
 ocupaContinente(Jugador, Continente) :-
 	jugador(Jugador),
 	continente(Continente),
 	forall(estaEn(Continente, Pais), ocupa(Pais, Jugador)).
-
